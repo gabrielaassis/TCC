504a2b250131089eb8e0a0fea6eef9fa09e50dfa
Leandro Neves <leandron07@gmail.com>
2018-10-13 11:38:35 -0300

    TelaTimes listando os times

--- app/src/main/java/com/example/appfut03/Aplicativo/TelaPartida.java
+++ app/src/main/java/com/example/appfut03/Aplicativo/TelaPartida.java
@@ -1,25 +1,34 @@
 package com.example.appfut03.Aplicativo;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.appfut03.Configuracoes.Firebaseconfig;
 import com.example.appfut03.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TelaPartida extends AppCompatActivity {
 
     private TextView txtSuaEquipe;
     private FirebaseAuth autentic;
     private DatabaseReference fbconfig;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,7 @@ public class TelaPartida extends AppCompatActivity {
         setContentView(R.layout.tela_partida);
 
         txtSuaEquipe = (TextView) findViewById(R.id.txtSuaEquipe);
+
 
         fbconfig = Firebaseconfig.getFirebaseConfig();
         autentic = Firebaseconfig.getFirebaseAutentic();
@@ -49,5 +59,46 @@ public class TelaPartida extends AppCompatActivity {
 
             }
         });
+
+        List<String> list = new ArrayList<String>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, list);
+        spinner.setAdapter(adapter);
+
+        fbconfig.child("TimesRegistrados").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String string = dataSnapshot.getValue(String.class);
+                if(string.equals("Visitante")){
+
+                }else {
+                    adapter.add(string);
+                    adapter.notifyDataSetChanged();
+
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        }
+
     }
-}
+
